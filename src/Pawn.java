c21ae1a97fad2b037ab68780b19dd44274b800d7
ESims7 <34346851+ESims7@users.noreply.github.com>
2018-11-09 11:03:55 +0000

    Pawn can no longer capture forward

--- src/Pawn.java
+++ src/Pawn.java
@@ -14,19 +14,19 @@ public class Pawn extends Piece {
         int changeInColumn = Math.abs(destinationColumn - column);
         int changeInRow = Math.abs(destinationRow - row);
 
-        boolean destinationSquareNotBlocked = !pieceOfSameColourAtDestination(destinationRow, destinationColumn, colour);
         boolean staysOnSameColumn = (changeInColumn == 0);
         boolean movesOneSquare = (changeInRow == 1);
         boolean movesTwoSquares = (changeInRow == 2);
         boolean isACaptureMove = changeInRow == 1 && changeInColumn == 1;
+        boolean destinationSquareNotBlocked = !pieceOfSameColourAtDestination(destinationRow, destinationColumn, colour);
         boolean oppositionPieceAtDestination = chessboard[destinationRow][destinationColumn] != null
-                && !chessboard[destinationRow][destinationColumn].getColour().equals(getPlayerToMove());
+                && !chessboard[destinationRow][destinationColumn].getColour().equals(colour);
         boolean squareInFrontBlocked = (colour.equals("White") && row != 0 && chessboard[row - 1][column] != null)
                 || (colour.equals("Black") && row != 7 && chessboard[row + 1][column] != null);
         boolean moveBlocked = movesTwoSquares && squareInFrontBlocked;
         boolean movingForward = colour.equals("White") ? destinationRow < row : destinationRow > row;
 
         return destinationSquareNotBlocked && movingForward && !moveBlocked
-                && ((isACaptureMove && oppositionPieceAtDestination) || (staysOnSameColumn  && ((!hasMoved && movesTwoSquares) || movesOneSquare)));
+                && ((isACaptureMove && oppositionPieceAtDestination) || (staysOnSameColumn  && ((!hasMoved && movesTwoSquares) || movesOneSquare)) && !oppositionPieceAtDestination);
     }
 }
